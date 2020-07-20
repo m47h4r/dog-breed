fcc34150f942eb8bbd3940d7d49f3ed69b255b61
m47h4r <m47h4r@gmail.com>
2020-07-20 14:46:18 +0430

    Updates

---
+++ precache-manifest.37476e8f3fd05c414291b0d40aea12cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "956d07d69beded507f2f649dcf915f14",
+    "url": "/dog-breed/index.html"
+  },
+  {
+    "revision": "237c50ec22af40dc930f",
+    "url": "/dog-breed/static/css/main.aec50abb.chunk.css"
+  },
+  {
+    "revision": "2dfde5d13e916e72cd9b",
+    "url": "/dog-breed/static/js/2.01443b19.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dog-breed/static/js/2.01443b19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "237c50ec22af40dc930f",
+    "url": "/dog-breed/static/js/main.36fdf491.chunk.js"
+  },
+  {
+    "revision": "43baa389bc2733a15ffb",
+    "url": "/dog-breed/static/js/runtime-main.a39a427e.js"
+  }
+]);
